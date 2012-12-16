02cb6ab99b94ded06a408372dbbef430ba5d9fe5
Gursimran Singh <g13singh@gmail.com>
2012-12-15 21:08:59 -0600

    made full paths the keys, made values another hash

--- repoman.rb
+++ repoman.rb
@@ -1,20 +1,20 @@
 require 'yaml'
+require 'pp'
 
 module RepoMan
   GIT_REPOS = "#{File.dirname(__FILE__)}/git-repos.yml"
 
   def self.handle options
-    if options.has_key? :add
-      add options[:add]
-    end
+    add! options[:add] if options[:add] != ''
+    list if options[:list]
   end
 
-  def add path
+  def add! path
     if File.directory? path and has_git? path
       full_path = File.expand_path(path)
       basename = File.basename(full_path)
       puts "repo: I'm adding '#{basename}' to '#{GIT_REPOS}'"
-      add_repo basename, full_path
+      add_repo! basename, full_path
       exit 0
     else
       STDERR.puts "repo: Not a git repository -- #{path}"
@@ -22,28 +22,46 @@ module RepoMan
     end
   end
 
-  def has_git? path
-    File.directory? "#{path}/.git"
+  def list
+    git_repos_validate
+    hash = YAML::load(File.read(GIT_REPOS))
+    longest_name = 0
+    hash.each do |k,v|
+     longest_name = v[:name].length if v[:name].length > longest_name 
+    end
+    hash.each do |key, value|
+     printf "%-#{longest_name}s %s\n", value[:name], key
+    end
+    exit 0
   end
 
-  def add_repo reponame, full_path
+  def add_repo! reponame, full_path
+    git_repos_validate
+    hash = YAML::load(File.read(GIT_REPOS))
+    hash = {} if hash==nil
+    hash[full_path.to_sym] = {:name => reponame}
+    File.open(GIT_REPOS, "w") do |f|
+      f.write hash.to_yaml
+    end
+  end
+
+  def git_repos_validate
     unless git_repos_exist?
       File.new(GIT_REPOS, "w")
       File.open(GIT_REPOS, "w") do |f|
         f.write "---\n"
       end
     end
-    hash = YAML::load(File.read(GIT_REPOS))
-    hash = {} if hash==nil
-    hash[reponame.to_sym] = full_path
-    File.open(GIT_REPOS, "w") do |f|
-      f.write hash.to_yaml
-    end
+  end
+
+  def has_git? path
+    File.directory? "#{path}/.git"
   end
 
   def git_repos_exist?
     a = File.exist? GIT_REPOS
-    b = File.read(GIT_REPOS)!='' 
+    b = File.read(GIT_REPOS)!='' if a
     a and b
   end
+
 end
